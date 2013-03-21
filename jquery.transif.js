7fc51a94d267258a66a21b081269d7826d23ab3c
mdix <dix@mediaman.de>
2013-03-21 11:16:41 +0100

    initial commit

---
+++ jquery.transif.js
@@ -0,0 +1,50 @@
+(function($) {
+    $.fn.transif = function(animationConfigObject, animationSpeed, callback) {
+        this._animationConfigObject = animationConfigObject;
+        this._animationSpeed = animationSpeed;
+        this._callback = callback;
+        
+        this.applyAppropriateAnimationTechnique = function() {
+            if (jQuery.data(document, 'supportsCssTransitions')) {
+                console.log('Using transition');
+                $(this).transition(
+                    this._animationConfigObject,
+                    this._animationSpeed,
+                    this._callback
+                );
+            } else {
+                console.log('Using animate');
+                $(this).animate(
+                    this._animationConfigObject,
+                    this._animationSpeed,
+                    this._callback
+                );
+            }
+        };
+        
+        this.applyAppropriateAnimationTechnique();
+    };
+    
+    // detection code based on http://stackoverflow.com/questions/7264899/detect-css-transitions-using-javascript-and-without-modernizr
+    (this.checkForCssTransitionSupport = function() {
+        var b = document.body || document.documentElement;
+        var s = b.style;
+        var p = 'transition';
+        if (typeof s[p] == 'string') {
+            jQuery.data(document, 'supportsCssTransitions', true);
+            return;
+        }
+
+        // Tests for vendor specific prop
+        v = ['Moz', 'Webkit', 'Khtml', 'O', 'ms'],
+        p = p.charAt(0).toUpperCase() + p.substr(1);
+        for (var i = 0; i < v.length; i++) {
+            if (typeof s[v[i] + p] == 'string') { 
+                jQuery.data(document, 'supportsCssTransitions', true);
+                return;
+            }
+        }
+        jQuery.data(document, 'supportsCssTransitions', false);
+        return;
+    }());
+}(jQuery));
